8df34ca978d9ccb55b48d2452b41be6a2ede65c8
Maxim <maksimkalin@mail.ru>
2023-07-12 18:24:05 +0300

    fix: изменил status type, вызывало ошибку

--- src/types/Main.ts
+++ src/types/Main.ts
@@ -2,13 +2,9 @@ export type TtoDoCategories = {
   name: string;
   bgColor: string;
 };
-export type Status = {
-  New: string;
-  InProcess: string;
-  Completed: string;
-};
+
 export type Data = {
   id: string;
   content: string;
-  status: Status;
+  status: string;
 };
